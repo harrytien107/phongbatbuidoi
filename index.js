1a74bc799af5233ad77440c2b703e97422340c3b
HaryTien <zeentien.123@gmail.com>
2024-12-15 16:09:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import jsonfile from 'jsonfile';
+import moment from 'moment';
+import simpleGit from 'simple-git';
+import { Random } from 'random';
+
+const path = './data.json';
+
+const isValidDate = (date) => {
+    const startDate = moment('2023-01-01');
+    const endDate = moment('2024-12-1');
+    return date.isBetween(startDate, endDate, null, '[]');
+};
+
+const markCommit = async (date) => {
+    const data = { date: date.toISOString() };
+    await jsonfile.writeFile(path, data);
+
+    const git = simpleGit();
+    await git.add(path);
+    await git.commit(date.toISOString()), { '--date': date.toISOString() };
+};
+
+const makeCommits = async () => {
+    const git = simpleGit();
+
+    for (let i=0; i < n; i++) {
+        const randomWeek = random.int(0, 54 * 4);
+        const randomDay = random.int(0, 6);
+
+        const randomDate = moment('2023-01-01')
+            .add(randomWeek, 'weeks')
+            .add(randomDay, 'days');
+
+        if (isValidDate(randomDate)) {
+            console.log('Creating commit: ${randomDate.toISOString()}');
+            await markCommit(randomDate);
+        } else {
+            console.log('Invalid date: ${randomDate.toISOString()}, skipping...');
+        }
+    }
+    console.log('Pushing all commits...');
+    await git.push();
+};
+
+makeCommits(50000);
